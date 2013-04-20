148451c2014a4afa0d85fe4ef417a4fa9d114b9e
DBC Apprentice <apprentice@devbootcamp.com>
2013-04-19 18:01:43 -0700

    workingish

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,21 @@
 get '/' do
   erb :index
 end
+
+post '/pull_updates' do
+  content_type :json
+  @game = Game.find(params[:id])
+  @game.state.to_json
+end
+
+post '/update_ruby' do
+  content_type :json
+  @game = Game.find(params[:id])
+  @game.update_attributes(:state => params[:state])
+  params[:state].to_json
+end
+
+get '/game/:id/:player' do
+  @game = Game.find_or_create_by_id(params[:id])
+  erb :game
+end
